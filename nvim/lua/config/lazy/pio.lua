d6c423d83648324deba44a56682d5effc8ffd6ab
Isaac Rowell <isaacgrowell@gmail.com>
2025-06-26 07:13:18 -0400

    Update pio.lua

--- nvim/lua/config/lazy/pio.lua
+++ nvim/lua/config/lazy/pio.lua
@@ -1,4 +1,9 @@
 return {
-   'anurag3301/nvim-platformio.lua',
-  -- cmd = { 'Pioinit', 'Piorun', 'Piocmdh', 'Piocmdf', 'Piolib', 'Piomon', 'Piodebug', 'Piodb' },
+    'anurag3301/nvim-platformio.lua',
+    -- cmd = { 'Pioinit', 'Piorun', 'Piocmdh', 'Piocmdf', 'Piolib', 'Piomon', 'Piodebug', 'Piodb' },
+    dependencies = {
+        { "akinsho/nvim-toggleterm.lua" },
+        { "nvim-telescope/telescope.nvim" },
+        { "nvim-lua/plenary.nvim" },
+    },
 }
