992152b02dfbc399bdc6f559e2f057eb7bc38c22
isaacgr <isaacgrowell@gmail.com>
2025-11-23 05:36:42 -0500

    Some dotfiles updates

--- nvim/lua/config/lazy/peek.lua
+++ nvim/lua/config/lazy/peek.lua
@@ -6,7 +6,8 @@ return {
         config = function()
             require("peek").setup({
                 filetype = { 'markdown', 'conf' },
-                app="browser"
+                app="browser",
+                theme='light'
             })
             vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
             vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
