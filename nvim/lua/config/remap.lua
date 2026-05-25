f4f864f606de6fda5dbb032a37001e7c6e8cdd47
irowell <irowell@evertz.com>
2026-05-25 05:34:34 -0400

    Add descriptions to keymaps

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,40 +1,40 @@
 -- Open netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {})
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw file explorer" })
 
 -- move highlighted line/block up/down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv", { desc = "Move selection up" })
 
 -- Join the current line with the next but keep the cursor in place
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines (cursor stays)" })
 
 -- Half-page jump up/down keeping cursor centered
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half-page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half-page up (centered)" })
 
 -- Search next/previous result and center on the screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
 
 -- Paste over selection without overwriting the default register
 -- This one is very good
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", "\"_dP", { desc = "Paste (preserve register)" })
 
 -- Yank whole line to system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-vim.keymap.set("v", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "Yank to clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Yank to clipboard" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "Yank line to clipboard" })
+vim.keymap.set("v", "<leader>Y", "\"+Y", { desc = "Yank line to clipboard" })
 
 -- Delete without affecting the default register
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete (no register)" })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Delete (no register)" })
 
 -- Disable annoying Ex mode key
-vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", "<nop>", { desc = "Disabled (Ex mode)" })
 
 -- Launch tmux-sessionizer in a new tmux window
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Tmux sessionizer" })
 
 -- Format buffer using LSP
 vim.keymap.set("n", "<leader>f", function()
@@ -46,19 +46,19 @@ vim.keymap.set("n", "<leader>f", function()
     else
         vim.lsp.buf.format({ async = true })
     end
-end
+end, { desc = "Format buffer" }
 )
 
 -- Jump to next/previous quickfix item and keep centered on screen
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Previous quickfix item" })
 
 -- Jump to next/previous location list item and keep centered on screen
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next location list item" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Previous location list item" })
 
 -- Search and replace word under the cursor globally, with confirmation
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "Search/replace word under cursor" })
 
 -- Make the current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
@@ -69,7 +69,7 @@ vim.keymap.set("n", "<leader>rm", ":%s/\\r//g<CR>", {
 })
 
 -- Pretty print JSON file in current buffer using python
-vim.keymap.set("n", "<leader>J", ":%!python3 -m json.tool<CR>")
+vim.keymap.set("n", "<leader>J", ":%!python3 -m json.tool<CR>", { desc = "Format JSON (python)" })
 
 -- Recursive grep for exact word under cursor (wrapped in \< \>)
 vim.keymap.set('n', '<C-g>',
@@ -82,7 +82,7 @@ vim.keymap.set('n', '<C-h>',
     { desc = "Grep symbol under cursor" })
 
 -- Disable arrow keys in normal, insert, and visual modes
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true, desc = "Disabled (use hjkl)" }
 vim.keymap.set({ "n", "i", "v" }, "<Up>", "<NOP>", opts)
 vim.keymap.set({ "n", "i", "v" }, "<Down>", "<NOP>", opts)
 vim.keymap.set({ "n", "i", "v" }, "<Left>", "<NOP>", opts)
